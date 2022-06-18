ef98d1d91a345b36f340b76eefebb7211a8d3b42
star-dark <74770897+star-dark@users.noreply.github.com>
2022-06-18 12:44:46 +0900

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -1,11 +1,34 @@
-const clock = document.querySelector("h2#clock");
-function getClock() {
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2,"0");
-    const minutes = String(date.getMinutes()).padStart(2,"0");
-    const seconds = String(date.getSeconds()).padStart(2,"0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const logOut = document.querySelector("#logOut");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreeting(username);
 }
 
-getClock();
-setInterval(getClock, 1000);
+function paintGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    logOut.classList.remove(HIDDEN_CLASSNAME);
+    logOut.addEventListener("click", RemoveUserName);
+}
+function RemoveUserName() {
+    localStorage.clear();
+    location.reload();
+}
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername===null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+    paintGreeting(savedUsername);
+}
