2e483994bb9712b0efffe608954e388a9132da04
star-dark <den1116@naver.com>
2022-06-19 14:26:24 +0900

    background setting

--- function/background.js
+++ function/background.js
@@ -1,1 +1,5 @@
-
+const img = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.JPG","10.JPG"];
+const backgroundImg = document.createElement("img")
+const ranimg = img[Math.floor(Math.random() * img.length)];
+backgroundImg.src = `./img/${ranimg}`;
+document.body.appendChild(backgroundImg);
